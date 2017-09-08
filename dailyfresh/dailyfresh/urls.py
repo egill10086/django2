257026cf836eab01cc2978b8a8d26dd413282f8f
egill10086 <Egill9527@163.com>
2017-09-08 21:19:18 +0800

    实验

--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/',include('uesr.html')),
+    url(r'^user/$',include('uesr.html')),
+    url(r'^',include('url.py'))
 ]
